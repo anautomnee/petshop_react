5e3140580bc5f072fb66dbfbb5c124d421ee5345
Anastasiia Kozlova <anautomnee@gmail.com>
2024-09-21 22:24:09 +0300

    add show more/less

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,7 +1,7 @@
 import { Box, Button, Stack, Typography } from "@mui/material"
 import { BreadcrumbsLine } from "../components/breadcrumbs/BreadcrumbsLine"
 import { MainLayout } from "../layouts/MainLayout"
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import { getDiscount } from "../utils";
@@ -13,6 +13,7 @@ import { addToCart } from "../store/productsSlice";
 export const Product = () => {
     const [product, setProduct] = useState({});
     const [categoryName, setCategoryName] = useState("");
+    const descriptionRef = useRef(null);
     const dispatch = useDispatch();
     const { productId } = useParams();
     const API_URL = "http://localhost:3333";
@@ -33,12 +34,22 @@ export const Product = () => {
         getProduct(productId)
     }, [productId]);
 
+    const readMore = (e) => {
+        descriptionRef.current.classList.toggle("description");
+        e.target.textContent === "Show less" ? e.target.textContent = "Read more" : e.target.textContent = "Show less";
+    }
+
     return <MainLayout>
         <div className="container">
             <BreadcrumbsLine breadcrumbs={[{ title: "Main page", url: "/" }, { title: "Categories", url: "/categories" }, { title: categoryName ? categoryName : "Category", url: `/categories/${product?.categoryId}` }]} current={product?.title?.slice(0, 18) + "..."} />
             <Stack direction="row" gap={4} mb={10}>
                 <Box sx={{ width: 548, height: 542 }}>
-                    <img style={{}} src={`${API_URL}/${product?.image}`} alt={`${product?.title?.slice(0, 18)}...`} />
+                    <img style={{
+                        objectFit: "contain",
+                        width: "100%",
+                        height: "100%",
+                        overflow: "hidden"
+                    }} src={`${API_URL}/${product?.image}`} alt={`${product?.title?.slice(0, 18)}...`} />
                 </Box>
                 <Stack>
                     <Typography mb={5} variant="h3" sx={{ maxWidth: 548 }}>{product?.title}</Typography>
@@ -54,8 +65,8 @@ export const Product = () => {
                         <Button variant="contained" onClick={() => dispatch(addToCart(product))} sx={{ width: 316, height: 58, ml: 4, fontSize: 20 }}>Add to cart</Button>
                     </Stack>
                     <Typography mb={2}>Description</Typography>
-                    <Typography variant="description">{product?.description}</Typography>
-                    <Typography mt={4} sx={{ textDecoration: "underline", fontSize: 16, cursor: "pointer" }}>Read more</Typography>
+                    <Typography className="description" ref={descriptionRef} variant="description">{product?.description}</Typography>
+                    <Typography onClick={(e) => readMore(e)} mt={4} sx={{ textDecoration: "underline", fontSize: 16, cursor: "pointer" }}>Read more</Typography>
                 </Stack>
             </Stack>
 
