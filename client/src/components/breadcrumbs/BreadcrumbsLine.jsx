8fa60d700c91b0e352c26aa85db429e228d6edae
Anastasiia Kozlova <anautomnee@gmail.com>
2024-10-01 13:27:12 +0200

    Apply suggestions to BreadcrumbsLine element to improve visibility and use ternar operator for condition

--- client/src/components/breadcrumbs/BreadcrumbsLine.jsx
+++ client/src/components/breadcrumbs/BreadcrumbsLine.jsx
@@ -1,26 +1,22 @@
-import { Box, Stack } from "@mui/material"
-import { Link } from "react-router-dom"
-import { LinkBorderBtn } from "../../theme/customComponents"
 import { useContext } from "react"
+import { Link } from "react-router-dom"
+import { Box, Stack } from "@mui/material"
+import { LinkBorderBtn } from "../../theme/customComponents"
 import { BreadcrumbsContext } from "../../context/breadcrumbsContext"
 
-export const BreadcrumbsLine = ({ breadcrumbs }) => {
+export const BreadcrumbsLine = () => {
     const { crumbs } = useContext(BreadcrumbsContext)
-    return <div className="container">
-        <Stack direction="row" alignItems="center" mt={5} mb={5} sx={{ flexWrap: 'wrap' }} rowGap={2}>
-            {crumbs.map((breadcrumb, ind) => {
-                if (!(ind === crumbs.length - 1)) {
-                    return <Stack direction="row" alignItems="center" key={ind}>
+    return (
+        <Stack className="container" direction="row" alignItems="center" mt={5} mb={5} sx={{ flexWrap: 'wrap' }} rowGap={2}>
+            {crumbs.map((breadcrumb, ind) =>
+                !(ind === crumbs.length - 1) ?
+                    <Stack direction="row" alignItems="center" key={ind}>
                         <Link underline="hover" color="inherit" to={breadcrumb.url}>
                             <LinkBorderBtn sx={{ color: ind === crumbs.length - 1 ? "#282828" : "#8B8B8B" }}>{breadcrumb.title}</LinkBorderBtn>
                         </Link>
                         {!(ind === crumbs.length - 1) && <Box sx={{ borderBottom: "1px solid #DDDDDD", width: 16 }} />}
-                    </Stack>
-                } else {
-                    return <LinkBorderBtn key={ind} sx={{ color: "#282828", cursor: "auto", "&:hover": { backgroundColor: "#FFFFFF" } }}>{breadcrumb.title}</LinkBorderBtn>
-                }
-            }
+                    </Stack> :
+                    <LinkBorderBtn key={ind} sx={{ color: "#282828", cursor: "auto", "&:hover": { backgroundColor: "#FFFFFF" } }}>{breadcrumb.title}</LinkBorderBtn>
             )}
-        </Stack>
-    </div>
+        </Stack>)
 }
